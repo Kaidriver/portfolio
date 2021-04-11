77feff0ce89998c69007dfe42a89c5ef85dca1af
Kaiwen Song <jirachiji@gmail.com>
2021-04-10 20:09:24 -0400

    Initial Commit - App setup

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+function App() {
+  return (
+    <Container>
+      <Button variant="primary">Primary</Button>
+    </Container>
+  );
+}
+
+export default App;
